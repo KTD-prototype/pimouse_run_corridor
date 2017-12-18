57af01c351f3497a2f0370d86edb87a2203fa213
Kazuho Kobayashi <hozu.kazu.hozu@gmail.com>
2017-12-18 16:16:10 +0000

    wrote scripts

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -1,6 +1,11 @@
 #!/bin/bash -xve
 
-#sync and make
-rsync -av ./ ~/catkin_ws/src/pimouse_ros/
+#ワークスペースにリポジトリをコピー
+rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
+
+#pimouse_rosをgit cloneでワークスペースにもってくる
+cd ~/catkin_ws/src/
+git clone --depth=1 https://github.com/k24koba/pimouse_ros.git
+
 cd ~/catkin_ws
 catkin_make
